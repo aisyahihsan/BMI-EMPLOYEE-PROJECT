fc252a8a7bc413182045baf21e85da0b6709f363
User <User@AISYAIHSAN>
2023-08-26 21:24:29 +0800

    Add project files.

---
+++ Test2001/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test2001.Models;
+
+namespace Test2001.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeCharity101()
+        {
+            IList<Employee101> listEmployees = new List<Employee101>()
+            {
+                new Employee101() { EmpId = "001", Name = "Jamal Nasir",Position = "Defender", 
+                    Campus = "MIIT", Salary = 4631},
+                new Employee101() { EmpId = "002", Name = "Mohkhtar Dahari",Position = "Striker",
+                    Campus = "BMI", Salary = 3850},
+                new Employee101() { EmpId = "003", Name = "Soh Chin Aun",Position = "Defender",
+                    Campus = "MFI", Salary = 10020},
+                new Employee101() { EmpId = "004", Name = "Santokh Singh",Position = "Defender",
+                    Campus = "BMI", Salary = 2050},
+                new Employee101() { EmpId = "005", Name = "R.Arumugam",Position = "Goalkeeper",
+                    Campus = "MIIT", Salary = 16205},
+                new Employee101() { EmpId = "006", Name = "Shukor Salleh",Position = "Midfielder",
+                    Campus = "MIIT", Salary = 6631},
+                new Employee101() { EmpId = "007", Name = "Hassani Sani",Position = "Striker",
+                    Campus = "MIIT", Salary = 5300}
+            };
+            ViewBag.OrderName = listEmployees.OrderBy(x => x.Name);
+
+            ViewBag.CountAll = listEmployees.Count();
+            ViewBag.HighSalary = listEmployees.Max(x => x.Salary);
+            ViewBag.LowSalary = listEmployees.Min(x => x.Salary);
+            ViewBag.AverageSalary = listEmployees.Average(x => x.Salary);
+            ViewBag.AverageSalaryMIIT = listEmployees.Where(x => x.Campus == "MIIT")
+               .Average(x => x.Salary);
+            return View(listEmployees);
+        }
+    }
+}
---
+++ Test2001/Models/Employee101.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test2001.Models
+{
+    public class Employee101
+    {
+        public string EmpId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Campus { get; set; }
+        public double Salary { get; set; }
+        public double Deduction
+        {
+            get
+            {
+                if (Salary < 8000)
+                {
+                    return Salary * 0.005;
+                }
+                else
+                {
+                    return Salary * 0.01;
+                }
+            }
+        }
+    }
+}
